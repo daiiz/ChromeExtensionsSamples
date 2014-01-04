f890d4375a4c83653c5bad3d97f731997b9fa28e
daiz <da.iz.mg@gmail.com>
2014-01-04 17:39:58 +0900

    first commit

---
+++ chrome.pageCapture/app.js
@@ -0,0 +1,15 @@
+﻿function demo() {
+   chrome.tabs.getSelected(function(tab) {
+       var id = tab.id;
+       chrome.pageCapture.saveAsMHTML({"tabId": id}, function(blob) {
+           var url = window.webkitURL.createObjectURL(blob);
+           var a = document.getElementById("dl");
+           a.href = url;
+           a.download = "page.mhtml";
+           a.style.display = "block";
+       })
+   });
+}
+
+
+window.addEventListener("load", demo, false);
