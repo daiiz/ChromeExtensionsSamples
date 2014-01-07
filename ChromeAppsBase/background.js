cf9d358558574209ccdb075d9a74bd746831eac1
daiz <da.iz.mg@gmail.com>
2014-01-07 19:57:18 +0900

    基本パッケージを追加

---
+++ ChromeAppsBase/background.js
@@ -0,0 +1,17 @@
+﻿// background.js
+chrome.app.runtime.onLaunched.addListener(function() {
+     // success
+         chrome.app.window.create('sources/main.html', {
+            maxWidth: 400,
+            maxHeight: 250,
+            type: 'shell',
+            id: '__DemoTaro__',
+            singleton: true
+         },function(appWindow) {
+                  appWindow.resizeTo(400, 250)
+         });
+});
+
+
+
+
---
+++ ChromeExtensionsBase/background.js
@@ -0,0 +1,17 @@
+﻿// background.js
+chrome.app.runtime.onLaunched.addListener(function() {
+     // success
+         chrome.app.window.create('sources/main.html', {
+            maxWidth: 710+18,
+            maxHeight: 587+40,
+            type: 'shell',
+            id: '__DemoTaro__',
+            singleton: true
+         },function(appWindow) {
+                  appWindow.resizeTo(710+18, 587+40);
+         });
+});
+
+
+
+
